9f8f0ede201ad5ec1e28455bc64f65f39ff213f3
Adrian Kunz <clashsoft@hotmail.com>
2020-11-13 18:55:41 +0100

    feat: Add initial server.py

---
+++ server.py
@@ -0,0 +1,83 @@
+from flask import Flask
+
+from wakeonlan import send_magic_packet
+from rpi_ws281x import *
+
+import time
+import os
+import sys
+
+sys.path.append(os.path.abspath('.'))
+
+# Server Configuration
+PORT = 8080
+
+# LED strip configuration:
+LED_COUNT = 300      # Number of LED pixels.
+LED_PIN = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+# True to invert the signal (when using NPN transistor level shift)
+LED_INVERT = False
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+def colorSet(strip, color):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+    strip.show()
+
+
+def colorWipe(strip, color, wait_ms=20):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+
+def rainbow(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i+j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+
+@app.route('/api/effect', methods=['POST'])
+def set_color():
+    body = request.json
+    effect = body['effect']
+    color = Color(body['r'], body['g'], body['b'])
+    if effect == 'set':
+        colorSet(strip, color)
+    elif effect == 'wipe':
+        colorWipe(strip, color)
+    else:
+        return {
+            'error': 'unknown effect',
+        }
+
+    return {}
+
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
+app = Flask(__name__)
